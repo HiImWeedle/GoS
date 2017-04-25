df5d11b8a975068bc54818bc7ef3e2ad166066c1
Weedle <Aspring@protonmail.com>
2017-04-25 18:00:23 +0200

    Update WeedleAIO.lua
    added karma qwe

--- WeedleAIO.lua
+++ WeedleAIO.lua
@@ -1197,11 +1197,22 @@ end
 function Karma:Menu()
 	KoreanMechanics.Spell:MenuElement({id = "Q", name = "Q Key", key = string.byte("Q")})
 	KoreanMechanics.Spell:MenuElement({id = "QR", name = "Q Range", value = 950, min = 0, max = 950, step = 10})
+	KoreanMechanics.Spell:MenuElement({id = "W", name = "W Key", key = string.byte("W")})		
+	KoreanMechanics.Spell:MenuElement({id = "E", name = "E Key", key = string.byte("E")})
+	KoreanMechanics.Spell:MenuElement({id = "EMode", name = "self E Toggle", key = string.byte("T"), toggle = true})	
 
 	KoreanMechanics.Draw:MenuElement({id = "QD", name = "Draw Q range", type = MENU})
     KoreanMechanics.Draw.QD:MenuElement({id = "Enabled", name = "Enabled", value = true})       
     KoreanMechanics.Draw.QD:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
     KoreanMechanics.Draw.QD:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})
+    KoreanMechanics.Draw:MenuElement({id = "WD", name = "Draw W range", type = MENU})
+    KoreanMechanics.Draw.WD:MenuElement({id = "Enabled", name = "Enabled", value = true})       
+    KoreanMechanics.Draw.WD:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
+    KoreanMechanics.Draw.WD:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})     
+    KoreanMechanics.Draw:MenuElement({id = "ED", name = "Draw E range", type = MENU})
+    KoreanMechanics.Draw.ED:MenuElement({id = "Enabled", name = "Enabled", value = true})       
+    KoreanMechanics.Draw.ED:MenuElement({id = "Width", name = "Width", value = 1, min = 1, max = 5, step = 1})
+    KoreanMechanics.Draw.ED:MenuElement({id = "Color", name = "Color", color = Draw.Color(255, 255, 255, 255)})     
 end
 
 function Karma:Tick()
@@ -1209,6 +1220,12 @@ function Karma:Tick()
 		if KoreanMechanics.Spell.Q:Value() then
 			self:Q()
 		end
+		if KoreanMechanics.Spell.W:Value() then
+			self:W()
+		end
+		if KoreanMechanics.Spell.E:Value() then
+			self:E()
+		end			
 	end
 end
 
@@ -1219,6 +1236,21 @@ if target == nil then return end
 	Control.CastSpell(HK_Q, pos)
 end	
 
+function Karma:W()
+local target =  _G.SDK.TargetSelector:GetTarget(775)
+if target == nil then return end 	
+	Control.CastSpell(HK_W, target)
+end
+
+function Karma:E()
+	if KoreanMechanics.Spell.EMode:Value() then
+		Control.CastSpell(HK_E, myHero)
+	end
+	Control.CastSpell(HK_E, mousePos)
+end
+
+
+
 function Karma:Draw()
 	if not myHero.dead then
 		if KoreanMechanics.Draw.Enabled:Value() then
@@ -1228,10 +1260,22 @@ function Karma:Draw()
 			end
 			if not KoreanMechanics.Spell.Enabled:Value() and KoreanMechanics.Draw.OFFDRAW:Value() then 
 				Draw.Text("Aimbot OFF", 20, textPos.x - 80, textPos.y + 40, Draw.Color(255, 255, 000, 000)) 
-			end 
+			end
+			if KoreanMechanics.Spell.EMode:Value() then
+				Draw.Text("Self Shield ON", 20, textPos.x - 80, textPos.y + 60, Draw.Color(255, 000, 255, 000)) 		
+			end
+			if not KoreanMechanics.Spell.EMode:Value()  then 
+				Draw.Text("Self Shield OFF", 20, textPos.x - 80, textPos.y + 60, Draw.Color(255, 255, 000, 000)) 
+			end 			 
 			if KoreanMechanics.Draw.QD.Enabled:Value() then
 			    Draw.Circle(myHero.pos, KoreanMechanics.Spell.QR:Value(), KoreanMechanics.Draw.QD.Width:Value(), KoreanMechanics.Draw.QD.Color:Value())
 			end
+	    	if KoreanMechanics.Draw.WD.Enabled:Value() then
+	    	    Draw.Circle(myHero.pos, 675, KoreanMechanics.Draw.WD.Width:Value(), KoreanMechanics.Draw.WD.Color:Value())
+	    	end			
+	    	if KoreanMechanics.Draw.ED.Enabled:Value() then
+	    	    Draw.Circle(myHero.pos, 800, KoreanMechanics.Draw.ED.Width:Value(), KoreanMechanics.Draw.ED.Color:Value())
+	    	end	 			
 		end
 	end
 end
