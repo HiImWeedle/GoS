506272941d248d202b1df95c84122d45a0982c51
Weedle <Aspring@protonmail.com>
2018-04-06 00:25:38 +0200

    Update winrate_script_data.lua

--- WinRate/winrate_script_data.lua
+++ WinRate/winrate_script_data.lua
@@ -1,7 +1,7 @@
 	_G.WR_SCRIPT_DATA = 
 		{
-			core_version = 1.00,
-			version      = 1.00,
+			core_version = "1.0.0",
+			version      = "1.0.0",
 			changelog    = 
 			[[
 				ChangeLog Text Here
